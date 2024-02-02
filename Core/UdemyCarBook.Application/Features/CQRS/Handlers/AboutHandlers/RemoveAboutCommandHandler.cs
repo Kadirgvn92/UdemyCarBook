01b9c505c14e7db6f1990d603a0ae1ee46613b0b
Kadirgvn92 <kdrgvndeveloper@gmail.com>
2024-02-02 17:26:52 +0300

    Lesson 14 : Handlers created and integrated

---
+++ Core/UdemyCarBook.Application/Features/CQRS/Handlers/AboutHandlers/RemoveAboutCommandHandler.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UdemyCarBook.Application.Features.CQRS.Commands.AboutCommands;
+using UdemyCarBook.Application.Interfaces;
+using UdemyCarBook.Domain.Entities;
+
+namespace UdemyCarBook.Application.Features.CQRS.Handlers.AboutHandlers;
+public class RemoveAboutCommandHandler
+{
+    private readonly IRepository<About> _aboutRepository;
+
+    public RemoveAboutCommandHandler(IRepository<About> aboutRepository)
+    {
+        _aboutRepository = aboutRepository;
+    }
+    public async Task Handle(RemoveAboutCommand command)
+    {
+        var values = await _aboutRepository.GetByIDAsync(command.Id);
+        await _aboutRepository.DeleteAsync(values);
+    }
+}
---
+++ Core/UdemyCarBook.Application/Features/CQRS/Handlers/AboutHandlers/UpdateAboutCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UdemyCarBook.Application.Features.CQRS.Commands.AboutCommands;
+using UdemyCarBook.Application.Interfaces;
+using UdemyCarBook.Domain.Entities;
+
+namespace UdemyCarBook.Application.Features.CQRS.Handlers.AboutHandlers;
+public class UpdateAboutCommandHandler
+{
+    private readonly IRepository<About> _aboutRepository;
+
+    public UpdateAboutCommandHandler(IRepository<About> aboutRepository)
+    {
+        _aboutRepository = aboutRepository;
+    }
+    public async Task Handle(UpdateAboutCommand command)
+    {
+        var values = await _aboutRepository.GetByIDAsync(command.AboutID);
+        values.Title = command.Title;
+        values.Description = command.Description;
+        values.ImageUrl = command.ImageUrl;
+        await _aboutRepository.UpdateAsync(values);
+    }
+}
