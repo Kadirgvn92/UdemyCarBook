57751ce2e180440ee47d0b9cade04869c8729c93
Kadirgvn92 <kdrgvndeveloper@gmail.com>
2024-02-08 22:31:48 +0300

    Lesson 19-1 : BrandHandlers completed

---
+++ Core/UdemyCarBook.Application/Features/CQRS/Commands/BrandCommands/RemoveBrandCommand.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UdemyCarBook.Application.Features.CQRS.Commands.BrandCommands;
+public class RemoveBrandCommand
+{
+    public RemoveBrandCommand(int id)
+    {
+        ID = id;
+    }
+
+    public int ID { get; set; }
+}
---
+++ Core/UdemyCarBook.Application/Features/CQRS/Handlers/BrandHandlers/GetBrandByIDQueryHandler.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UdemyCarBook.Application.Features.CQRS.Queries.BrandQueries;
+using UdemyCarBook.Application.Features.CQRS.Results.BrandResults;
+using UdemyCarBook.Application.Interfaces;
+using UdemyCarBook.Domain.Entities;
+
+namespace UdemyCarBook.Application.Features.CQRS.Handlers.BrandHandlers;
+public class GetBrandByIDQueryHandler
+{
+    private readonly IRepository<Brand> _repository;
+
+    public GetBrandByIDQueryHandler(IRepository<Brand> repository)
+    {
+        _repository = repository;
+    }
+    public async Task<GetBrandByIDQueryResult> Handle(GetBrandByIDQuery query)
+    {
+        var values = await _repository.GetByIDAsync(query.ID);
+        return new GetBrandByIDQueryResult
+        {
+            BrandID = values.BrandID,
+            Name = values.Name
+        };
+    }
+}
