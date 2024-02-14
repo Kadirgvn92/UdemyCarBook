7daaea77db16613640d007ffcef4fe3510ba6dc2
Kadirgvn92 <kdrgvndeveloper@gmail.com>
2024-02-14 14:10:23 +0300

    Lesson 24 - 1 : Category entity cqrs process contineued

---
+++ Core/UdemyCarBook.Application/Features/CQRS/Commands/CategoryCommands/RemoveCategoryCommand.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UdemyCarBook.Application.Features.CQRS.Commands.CategoryCommands;
+public class RemoveCategoryCommand
+{
+    public RemoveCategoryCommand(int ıD)
+    {
+        ID = ıD;
+    }
+
+    public int ID { get; set; }
+}
---
+++ Core/UdemyCarBook.Application/Features/CQRS/Queries/CategoryQueries/GetCategoryByIDQuery.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UdemyCarBook.Application.Features.CQRS.Queries.CategoryQueries;
+public class GetCategoryByIDQuery
+{
+    public GetCategoryByIDQuery(int ıD)
+    {
+        ID = ıD;
+    }
+
+    public int ID { get; set; }
+}
