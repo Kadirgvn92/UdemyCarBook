3237007004fe057d176430a37357c0d33fbaf1ee
Kadirgvn92 <kdrgvndeveloper@gmail.com>
2024-03-18 17:46:36 +0300

    Footer commands created

---
+++ Core/UdemyCarBook.Application/Features/Mediator/Commands/FooterAddressCommands/RemoveFooterAddressCommand.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UdemyCarBook.Application.Features.Mediator.Commands.FooterAddressCommands;
+public class RemoveFooterAddressCommand : IRequest
+{
+    public RemoveFooterAddressCommand(int id)
+    {
+        Id = id;
+    }
+
+    public int Id { get; set; }
+}
