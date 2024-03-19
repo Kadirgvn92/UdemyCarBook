d751ed5c0e03034e90763c8ef0e5c39e693d5700
Kadirgvn92 <kdrgvndeveloper@gmail.com>
2024-03-19 10:42:22 +0300

    Lesson 35 - 36 : Location cqrs pattern about to finish

---
+++ Core/UdemyCarBook.Application/Features/Mediator/Handlers/LocationHandlers/GetLocationByIdQueryHandler.cs
@@ -0,0 +1,32 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UdemyCarBook.Application.Features.Mediator.Queries.LocationQueries;
+using UdemyCarBook.Application.Features.Mediator.Results.FooterAddressResult;
+using UdemyCarBook.Application.Features.Mediator.Results.LocationResults;
+using UdemyCarBook.Application.Interfaces;
+using UdemyCarBook.Domain.Entities;
+
+namespace UdemyCarBook.Application.Features.Mediator.Handlers.LocationHandlers;
+public class GetLocationByIdQueryHandler : IRequestHandler<GetLocationByIdQuery, GetLocationByIdQueryResult>
+{
+    private readonly IRepository<Location> _locationRepository;
+
+    public GetLocationByIdQueryHandler(IRepository<Location> locationRepository)
+    {
+        _locationRepository = locationRepository;
+    }
+
+    public async Task<GetLocationByIdQueryResult> Handle(GetLocationByIdQuery request, CancellationToken cancellationToken)
+    {
+        var values = await _locationRepository.GetByIDAsync(request.Id);
+        return new GetLocationByIdQueryResult
+        {
+            LocationID = values.LocationID,
+            Name = values.Name,
+        };
+    }
+}
---
+++ Core/UdemyCarBook.Application/Features/Mediator/Handlers/LocationHandlers/GetLocationQueryHandler.cs
@@ -0,0 +1,32 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UdemyCarBook.Application.Features.Mediator.Queries.LocationQueries;
+using UdemyCarBook.Application.Features.Mediator.Results.FooterAddressResult;
+using UdemyCarBook.Application.Features.Mediator.Results.LocationResults;
+using UdemyCarBook.Application.Interfaces;
+using UdemyCarBook.Domain.Entities;
+
+namespace UdemyCarBook.Application.Features.Mediator.Handlers.LocationHandlers;
+public class GetLocationQueryHandler : IRequestHandler<GetLocationQuery, List<GetLocationQueryResult>>
+{
+    private readonly IRepository<Location> _locationRepository;
+
+    public GetLocationQueryHandler(IRepository<Location> locationRepository)
+    {
+        _locationRepository = locationRepository;
+    }
+
+    public async Task<List<GetLocationQueryResult>> Handle(GetLocationQuery request, CancellationToken cancellationToken)
+    {
+        var values = await _locationRepository.GetAllAsync();
+        return values.Select(x => new GetLocationQueryResult
+        {
+           Name = x.Name,
+           LocationID = x.LocationID,
+        }).ToList();
+    }
+}
