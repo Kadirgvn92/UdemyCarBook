5db039dd5f6c99a0b54fc70e434eab1b2c38cddb
Kadirgvn92 <kdrgvndeveloper@gmail.com>
2024-03-19 11:58:45 +0300

    Lesson 38 : Pricing CQRS process handled

---
+++ Core/UdemyCarBook.Application/Features/Mediator/Handlers/PricingHandlers/GetPricingByIdQueryHandler.cs
@@ -0,0 +1,31 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UdemyCarBook.Application.Features.Mediator.Queries.PricingQueries;
+using UdemyCarBook.Application.Features.Mediator.Results.PricingResults;
+using UdemyCarBook.Application.Interfaces;
+using UdemyCarBook.Domain.Entities;
+
+namespace UdemyCarBook.Application.Features.Mediator.Handlers.PricingHandlers;
+public class GetPricingByIdQueryHandler : IRequestHandler<GetPricingByIdQuery, GetPricingByIdQueryResult>
+{
+    private readonly IRepository<Pricing> _pricingRepository;
+
+    public GetPricingByIdQueryHandler(IRepository<Pricing> pricingRepository)
+    {
+        _pricingRepository = pricingRepository;
+    }
+
+    public async Task<GetPricingByIdQueryResult> Handle(GetPricingByIdQuery request, CancellationToken cancellationToken)
+    {
+        var values = await _pricingRepository.GetByIDAsync(request.Id);
+        return new GetPricingByIdQueryResult
+        {
+            Name = values.Name,
+            PricingID = values.PricingID,
+        };
+    }
+}
---
+++ Core/UdemyCarBook.Application/Features/Mediator/Handlers/PricingHandlers/RemovePricingCommandHandler.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UdemyCarBook.Application.Features.Mediator.Commands.PricingCommands;
+using UdemyCarBook.Application.Interfaces;
+using UdemyCarBook.Domain.Entities;
+
+namespace UdemyCarBook.Application.Features.Mediator.Handlers.PricingHandlers;
+public class RemovePricingCommandHandler : IRequestHandler<RemovePricingCommand>
+{
+    private readonly IRepository<Pricing> _repository;
+
+    public RemovePricingCommandHandler(IRepository<Pricing> repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task Handle(RemovePricingCommand request, CancellationToken cancellationToken)
+    {
+       var values = await _repository.GetByIDAsync(request.Id);
+        await _repository.DeleteAsync(values);
+    }
+}
