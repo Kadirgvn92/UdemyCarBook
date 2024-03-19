b0d7c26d74a63e6b22f15055483646aacdefcff8
Kadirgvn92 <kdrgvndeveloper@gmail.com>
2024-03-19 09:32:15 +0300

    Lesson 33 : Remove and Update process completed footeraddress

--- Core/UdemyCarBook.Application/Features/Mediator/Handlers/FooterAddressHandlers/RemoveFooterAddressCommandHandler.cs
+++ Core/UdemyCarBook.Application/Features/Mediator/Handlers/FooterAddressHandlers/RemoveFooterAddressCommandHandler.cs
@@ -1,10 +1,26 @@
-﻿using System;
+﻿using MediatR;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UdemyCarBook.Application.Features.Mediator.Commands.FooterAddressCommands;
+using UdemyCarBook.Application.Interfaces;
+using UdemyCarBook.Domain.Entities;
 
 namespace UdemyCarBook.Application.Features.Mediator.Handlers.FooterAddressHandlers;
-public class RemoveFooterAddressCommandHandler
+public class RemoveFooterAddressCommandHandler : IRequestHandler<RemoveFooterAddressCommand>
 {
+    private readonly IRepository<FooterAddress> _repository;
+
+    public RemoveFooterAddressCommandHandler(IRepository<FooterAddress> repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task Handle(RemoveFooterAddressCommand request, CancellationToken cancellationToken)
+    {
+        var values = await _repository.GetByIDAsync(request.Id);
+        await _repository.DeleteAsync(values);
+    }
 }
