ee2c7f6d37d9935a52fbde308082854605ccaf23
Kadirgvn92 <kdrgvndeveloper@gmail.com>
2024-04-12 16:57:09 +0300

    Social Media Mediator classes created

---
+++ Core/UdemyCarBook.Application/Features/Mediator/Queries/SocialMediaQueries/GetSocialMediaByIdQueryResult.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UdemyCarBook.Application.Features.Mediator.Queries.SocialMediaQueries;
+public class GetSocialMediaByIdQueryResult : IRequest<GetSocialMediaByIdQueryResult>
+{
+    public int Id { get; set; }
+
+    public GetSocialMediaByIdQueryResult(int id)
+    {
+        Id = id;
+    }
+}
