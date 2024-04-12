b479dde7bf173843c49b3f8084682c3985a3c351
Kadirgvn92 <kdrgvndeveloper@gmail.com>
2024-04-12 17:52:47 +0300

    Lesson 44 - 45 - 46 - 47 : All ViewComponents completed

---
+++ Frontend/UdemyCarBook.WebUI/ViewComponents/UILayoutViewComponents/_HeadComponentPartial.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UdemyCarBook.WebUI.ViewComponents.UILayoutViewComponents;
+
+public class _HeadComponentPartial : ViewComponent
+{
+    public IViewComponentResult Invoke()
+    {
+        return View();
+    }
+}
