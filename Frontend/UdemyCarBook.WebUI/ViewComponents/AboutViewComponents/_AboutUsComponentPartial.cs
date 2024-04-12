b309be516a50cada4e164df0cab2413cb74b1784
Kadirgvn92 <kdrgvndeveloper@gmail.com>
2024-04-12 19:15:54 +0300

    Lesson 49 : About page API consume process started

--- Frontend/UdemyCarBook.WebUI/ViewComponents/AboutViewComponents/_AboutUsComponentPartial.cs
+++ Frontend/UdemyCarBook.WebUI/ViewComponents/AboutViewComponents/_AboutUsComponentPartial.cs
@@ -1,11 +1,26 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace UdemyCarBook.WebUI.ViewComponents.AboutViewComponents;
 
 public class _AboutUsComponentPartial : ViewComponent
 {
-    public IViewComponentResult Invoke()
+    private readonly IHttpClientFactory _httpClientFactory;
+
+    public _AboutUsComponentPartial(IHttpClientFactory httpClientFactory)
     {
+        _httpClientFactory = httpClientFactory;
+    }
+
+    public async Task< IViewComponentResult> InvokeAsync()
+    {
+        var clients = _httpClientFactory.CreateClient();
+        var responseMessage = await clients.GetAsync("https://localhost:7259/api/About");
+        if (responseMessage.IsSuccessStatusCode)
+        {
+            var jsonData = await responseMessage.Content.ReadAsStringAsync();   
+            //var values = JsonConvert.DeserializeObject<List<>>(jsonData);
+        }
         return View();
     }
 }
