c298dfdc081ca0af5aebf331d384a9bbf7362558
Kadirgvn92 <kdrgvndeveloper@gmail.com>
2024-04-12 19:05:16 +0300

    Lesson 48 : About view component created and tested

---
+++ Frontend/UdemyCarBook.WebUI/ViewComponents/AboutViewComponents/_AboutUsComponentPartial.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UdemyCarBook.WebUI.ViewComponents.AboutViewComponents;
+
+public class _AboutUsComponentPartial : ViewComponent
+{
+    public IViewComponentResult Invoke()
+    {
+        return View();
+    }
+}
