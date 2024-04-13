db0221d46bf4963ef3daf0321426587d3697bec7
Kadirgvn92 <kdrgvndeveloper@gmail.com>
2024-04-13 12:38:35 +0300

    Lesson 55 : Service API Consumed and tested

---
+++ Frontend/UdemyCarBook.DTO/ServiceDTOs/ResultServiceDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UdemyCarBook.DTO.ServiceDTOs;
+public class ResultServiceDTO
+{
+
+    public int ServiceID { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public string Icon { get; set; }
+}
