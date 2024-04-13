07fcebe44dfda15d15ae4c41d4d6becb00335aff
Kadirgvn92 <kdrgvndeveloper@gmail.com>
2024-04-13 10:55:51 +0300

    Lesson 52 : Become a driver section completed and Testimonail cqrs started

---
+++ Frontend/UdemyCarBook.WebUI/ViewComponents/TestimonialViewComponents/_TestimonialComponentPartial.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace UdemyCarBook.WebUI.ViewComponents.TestimonialViewComponents;
+
+public class _TestimonialComponentPartial : ViewComponent
+{
+    private readonly IHttpClientFactory _httpClientFactory;
+
+    public _TestimonialComponentPartial(IHttpClientFactory httpClientFactory)
+    {
+        _httpClientFactory = httpClientFactory;
+    }
+
+    public async Task<IViewComponentResult> InvokeAsync()
+    {
+        var client = _httpClientFactory.CreateClient();
+        var responseMessage = await client.GetAsync("https://localhost:7259/api/Testimonials");
+        if (responseMessage.IsSuccessStatusCode)
+        {
+            var jsonData = await responseMessage.Content.ReadAsStringAsync();
+            //var values = JsonConvert.DeserializeObject<List<>>(jsonData);
+        }
+        return View();
+    }
+}
