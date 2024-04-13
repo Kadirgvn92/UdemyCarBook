852265913c02012ba4c8bcc39b6356775aac3b2c
Kadirgvn92 <kdrgvndeveloper@gmail.com>
2024-04-13 12:59:51 +0300

    Lesson 56 : Car API's consumed and tested

---
+++ Frontend/UdemyCarBook.DTO/CarDTOs/ResultCarDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UdemyCarBook.DTO.CarDTOs;
+public class ResultCarDTO
+{
+    public int CarID { get; set; }
+    public int BrandID { get; set; }
+    public string BrandName { get; set; }
+    public string Model { get; set; }
+    public string CoverImageUrl { get; set; }
+    public int Km { get; set; }
+    public string Transmission { get; set; }
+    public short Seats { get; set; }
+    public short Luggage { get; set; }
+    public string Fuel { get; set; }
+    public string BigImageUrl { get; set; }
+}
---
+++ Frontend/UdemyCarBook.WebUI/Controllers/CarController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using UdemyCarBook.DTO.CarDTOs;
+using UdemyCarBook.DTO.ServiceDTOs;
+
+namespace UdemyCarBook.WebUI.Controllers;
+public class CarController : Controller
+{
+    private readonly IHttpClientFactory _httpClientFactory;
+
+    public CarController(IHttpClientFactory httpClientFactory)
+    {
+        _httpClientFactory = httpClientFactory;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        var client = _httpClientFactory.CreateClient();
+        var responseMessage = await client.GetAsync("https://localhost:7259/api/Car/GetCarWithBrand");
+        if (responseMessage.IsSuccessStatusCode)
+        {
+            var jsonData = await responseMessage.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<List<ResultCarDTO>>(jsonData);
+            return View(values);
+        }
+        return View();
+    }
+}
