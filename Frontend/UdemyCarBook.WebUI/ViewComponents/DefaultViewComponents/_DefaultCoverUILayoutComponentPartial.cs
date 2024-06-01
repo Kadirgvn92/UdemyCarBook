5f994864b4a2adf36c7a3be5a60230b36897b612
Kadirgvn92 <kdrgvndeveloper@gmail.com>
2024-06-01 23:58:41 +0300

    Lesson 59 : Default Page started to desinged

---
+++ Frontend/UdemyCarBook.WebUI/ViewComponents/DefaultViewComponents/_DefaultCoverUILayoutComponentPartial.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using UdemyCarBook.DTO.AboutDTOs;
+using UdemyCarBook.DTO.BannerDTOs;
+
+namespace UdemyCarBook.WebUI.ViewComponents.DefaultViewComponents;
+
+public class _DefaultCoverUILayoutComponentPartial :ViewComponent
+{
+    private readonly IHttpClientFactory _httpClientFactory;
+
+    public _DefaultCoverUILayoutComponentPartial(IHttpClientFactory httpClientFactory)
+    {
+        _httpClientFactory = httpClientFactory;
+    }
+
+    public async Task<IViewComponentResult> InvokeAsync()
+    {
+        var clients = _httpClientFactory.CreateClient();
+        var responseMessage = await clients.GetAsync("https://localhost:7259/api/Banner");
+        if (responseMessage.IsSuccessStatusCode)
+        {
+            var jsonData = await responseMessage.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<List<ResultBannerDto>>(jsonData);
+            return View(values);
+        }
+        return View();
+    }
+}
---
+++ Frontend/UdemyCarBook.WebUI/ViewComponents/UILayoutViewComponents/_DefaultNavbarComponentPartial.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UdemyCarBook.WebUI.ViewComponents.UILayoutViewComponents;
+
+public class _DefaultNavbarComponentPartial : ViewComponent
+{
+  public IViewComponentResult Invoke()
+    {
+        return View();
+    }
+}
