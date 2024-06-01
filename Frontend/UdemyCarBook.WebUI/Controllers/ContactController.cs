35fdcea6098182aec4879e255dcde90bc4abc9be
Kadirgvn92 <kdrgvndeveloper@gmail.com>
2024-06-01 23:26:26 +0300

    Lesson 57 : Footer Address API consumed

---
+++ Frontend/UdemyCarBook.WebUI/Controllers/ContactController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UdemyCarBook.WebUI.Controllers;
+public class ContactController : Controller
+{
+    private readonly IHttpClientFactory _httpClient;
+
+    public ContactController(IHttpClientFactory httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    public IActionResult Index()
+    {
+
+        return View();
+    }
+}
