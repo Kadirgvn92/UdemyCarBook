61049e6dac4841c20c62bd0e965b957ad58e150a
Kadirgvn92 <kdrgvndeveloper@gmail.com>
2024-06-09 17:46:49 +0300

    Lesson 62 : Last 5 cars api consumed

---
+++ Frontend/UdemyCarBook.WebUI/ViewComponents/DefaultViewComponents/_DefaultLast5CarsWithBrandsComponentPartial.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using UdemyCarBook.DTO.BannerDTOs;
+using UdemyCarBook.DTO.CarDTOs;
+
+namespace UdemyCarBook.WebUI.ViewComponents.DefaultViewComponents;
+
+public class _DefaultLast5CarsWithBrandsComponentPartial :ViewComponent
+{
+    private readonly IHttpClientFactory _httpClientFactory;
+
+    public _DefaultLast5CarsWithBrandsComponentPartial(IHttpClientFactory httpClientFactory)
+    {
+        _httpClientFactory = httpClientFactory;
+    }
+
+    public async Task<IViewComponentResult> InvokeAsync()
+    {
+        var clients = _httpClientFactory.CreateClient();
+        var responseMessage = await clients.GetAsync("https://localhost:7259/api/Car/GetLast5CarWithBrand");
+        if (responseMessage.IsSuccessStatusCode)
+        {
+            var jsonData = await responseMessage.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<List<ResultLast5CarsWithBrandsDto>>(jsonData);
+            return View(values);
+        }
+        return View();
+    }
+}
