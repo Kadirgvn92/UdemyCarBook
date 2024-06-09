09c1ce6cb7f73c836e00005810bf08e72020d559
Kadirgvn92 <kdrgvndeveloper@gmail.com>
2024-06-09 17:53:08 +0300

    Lesson 63 : Services taked to view component and viewed in Home page

---
+++ Frontend/UdemyCarBook.WebUI/ViewComponents/ServiceComponents/_ServiceComponentPartial.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using UdemyCarBook.DTO.CarDTOs;
+using UdemyCarBook.DTO.ServiceDTOs;
+
+namespace UdemyCarBook.WebUI.ViewComponents.ServiceComponents;
+
+public class _ServiceComponentPartial : ViewComponent
+{
+    private readonly IHttpClientFactory _httpClientFactory;
+
+    public _ServiceComponentPartial(IHttpClientFactory httpClientFactory)
+    {
+        _httpClientFactory = httpClientFactory;
+    }
+
+    public async Task<IViewComponentResult> InvokeAsync()
+    {
+        var client = _httpClientFactory.CreateClient();
+        var responseMessage = await client.GetAsync("https://localhost:7259/api/Services");
+        if (responseMessage.IsSuccessStatusCode)
+        {
+            var jsonData = await responseMessage.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<List<ResultServiceDTO>>(jsonData);
+            return View(values);
+        }
+        return View();
+    }
+}
