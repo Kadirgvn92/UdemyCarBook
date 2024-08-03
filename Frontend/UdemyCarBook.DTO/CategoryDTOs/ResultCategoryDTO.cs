47f8d2c352e7128f5184579ea5ee5191506936c0
Kadirgvn92 <kdrgvndeveloper@gmail.com>
2024-08-03 19:58:10 +0300

    Lesson 87 : BlogDetail Recent Blogs listed

---
+++ Frontend/UdemyCarBook.DTO/CategoryDTOs/ResultCategoryDTO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UdemyCarBook.DTO.CategoryDTOs;
+public class ResultCategoryDTO
+{
+    public int categoryID { get; set; }
+    public string name { get; set; }
+}
--- Frontend/UdemyCarBook.WebUI/ViewComponents/BlogViewComponents/_BlogDetailsCategoryViewComponentPartial.cs
+++ Frontend/UdemyCarBook.WebUI/ViewComponents/BlogViewComponents/_BlogDetailsCategoryViewComponentPartial.cs
@@ -1,11 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using UdemyCarBook.DTO.BlogDTOs;
+using UdemyCarBook.DTO.CategoryDTOs;
 
 namespace UdemyCarBook.WebUI.ViewComponents.BlogViewComponents;
 
 public class _BlogDetailsCategoryViewComponentPartial :ViewComponent
 {
-    public IViewComponentResult Invoke()
+    private readonly IHttpClientFactory _httpClientFactory;
+
+    public _BlogDetailsCategoryViewComponentPartial(IHttpClientFactory httpClientFactory)
     {
+        _httpClientFactory = httpClientFactory;
+    }
+    public async Task<IViewComponentResult> InvokeAsync()
+    {
+        var client = _httpClientFactory.CreateClient();
+        var respond = await client.GetAsync("https://localhost:44323/api/Categories");
+        if (respond.IsSuccessStatusCode)
+        {
+            var jsonData = await respond.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<List<ResultCategoryDTO>>(jsonData);
+            return View(values);
+        }
         return View();
     }
 }
