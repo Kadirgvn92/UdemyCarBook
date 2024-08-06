f5348c86d6e0e75040eb6df59101872af1e65ebc
Kadirgvn92 <kdrgvndeveloper@gmail.com>
2024-08-06 15:49:49 +0300

    Lesson 118 : CarFeature Page designed

---
+++ Frontend/UdemyCarBook.DTO/FeatureDTOs/ResultFeatureDTO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UdemyCarBook.DTO.FeatureDTOs;
+public class ResultFeatureDTO
+{
+    public int FeatureID { get; set; }
+    public string Name { get; set; }
+}
---
+++ Frontend/UdemyCarBook.WebUI/Controllers/AdminFeatureController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net.Http;
+using UdemyCarBook.DTO.CarDTOs;
+using UdemyCarBook.DTO.FeatureDTOs;
+
+namespace UdemyCarBook.WebUI.Controllers;
+public class AdminFeatureController : Controller
+{
+    private readonly IHttpClientFactory _httpClientFactory;
+
+    public AdminFeatureController(IHttpClientFactory httpClientFactory)
+    {
+        _httpClientFactory = httpClientFactory;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        var client = _httpClientFactory.CreateClient();
+        var responseMessage = await client.GetAsync("https://localhost:44323/api/Feature");
+        if (responseMessage.IsSuccessStatusCode)
+        {
+            var jsonData = await responseMessage.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<List<ResultFeatureDTO>>(jsonData);
+            return View(values);
+        }
+        return View();
+    }
+}
