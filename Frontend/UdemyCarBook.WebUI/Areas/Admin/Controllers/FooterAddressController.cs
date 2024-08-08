d667908ebfdd78e8f45ff604d0da1ae897b9e72a
Kadirgvn92 <kdrgvndeveloper@gmail.com>
2024-08-08 16:52:24 +0300

    Lesson 141 : FooterAddress complted admin with Modal

---
+++ Frontend/UdemyCarBook.WebUI/Areas/Admin/Controllers/FooterAddressController.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
+using UdemyCarBook.DTO.AboutDTOs;
+using UdemyCarBook.DTO.FooterAddressDTOs;
+
+namespace UdemyCarBook.WebUI.Areas.Admin.Controllers;
+
+[Area("Admin")]
+[Route("Admin/[controller]/[action]/{id?}")]
+public class FooterAddressController : Controller
+{
+    private readonly IHttpClientFactory _httpClientFactory;
+
+    public FooterAddressController(IHttpClientFactory httpClientFactory)
+    {
+        _httpClientFactory = httpClientFactory;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        var client = _httpClientFactory.CreateClient();
+        var responseMessage = await client.GetAsync("https://localhost:44323/api/FooterAddresses");
+        if (responseMessage.IsSuccessStatusCode)
+        {
+            var jsonData = await responseMessage.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<List<ResultFooterAddressDto>>(jsonData);
+            return View(values);
+        }
+        return View();
+    }
+    public async Task<IActionResult> Delete(int id)
+    {
+        var client = _httpClientFactory.CreateClient();
+        var responseMessage = await client.DeleteAsync($"https://localhost:44323/api/FooterAddresses?id={id}");
+        if (responseMessage.IsSuccessStatusCode)
+        {
+            return RedirectToAction("Index");
+        }
+        return View();
+    }
+    [HttpGet]
+    public async Task<IActionResult> Update(int id)
+    {
+        var client = _httpClientFactory.CreateClient();
+        var responseMessage = await client.GetAsync($"https://localhost:44323/api/FooterAddresses/{id}");
+        if (responseMessage.IsSuccessStatusCode)
+        {
+            var jsonData = await responseMessage.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<UpdateFooterAddressDTO>(jsonData);
+            return View(values);
+        }
+        return View();
+    }
+    [HttpGet]
+    public async Task<JsonResult> GetFooterAddress(int id)
+    {
+        var client = _httpClientFactory.CreateClient();
+        var responseMessage = await client.GetAsync($"https://localhost:44323/api/FooterAddresses/{id}");
+        if (responseMessage.IsSuccessStatusCode)
+        {
+            var jsonData = await responseMessage.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<UpdateFooterAddressDTO>(jsonData);
+            return Json(values);
+        }
+        return Json(null);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Update(UpdateFooterAddressDTO DTO)
+    {
+        var client = _httpClientFactory.CreateClient();
+        var jsonData = JsonConvert.SerializeObject(DTO);
+        StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+        var responseMessage = await client.PutAsync($"https://localhost:44323/api/FooterAddresses/", stringContent);
+
+        if (responseMessage.IsSuccessStatusCode)
+        {
+            return RedirectToAction("Index");
+        }
+
+        // Hata durumu için güncellenmiş verileri geri döndürebilirsiniz
+        return View(DTO);
+    }
+
+}
