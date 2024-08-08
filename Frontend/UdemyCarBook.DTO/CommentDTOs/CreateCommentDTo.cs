a6cab8fa6c15359dada4d33c85a2405323df459b
Kadirgvn92 <kdrgvndeveloper@gmail.com>
2024-08-08 15:37:45 +0300

    Lesson 136-137-138 : Comment completed

---
+++ Frontend/UdemyCarBook.DTO/CommentDTOs/CreateCommentDTo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UdemyCarBook.DTO.CommentDTOs;
+public class CreateCommentDTo
+{
+    public string Name { get; set; }
+    public DateTime CreatedDate { get; set; }
+    public string Description { get; set; }
+    public int BlogID { get; set; }
+}
---
+++ Frontend/UdemyCarBook.DTO/CommentDTOs/UpdateCommentDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UdemyCarBook.DTO.CommentDTOs;
+public class UpdateCommentDTO
+{
+    public int CommentId { get; set; }
+    public string Name { get; set; }
+    public DateTime CreatedDate { get; set; }
+    public string Description { get; set; }
+    public int BlogID { get; set; }
+}
