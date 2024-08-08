b3f075a17b0c525b918587855317fa5bdb00ce9d
Kadirgvn92 <kdrgvndeveloper@gmail.com>
2024-08-08 15:52:44 +0300

    Lesson 139 : Pricing completed

---
+++ Frontend/UdemyCarBook.DTO/PricingDTOs/CreatePricingDTO.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UdemyCarBook.DTO.PricingDTOs;
+public class CreatePricingDTO
+{
+    public string Name { get; set; }
+}
---
+++ Frontend/UdemyCarBook.DTO/PricingDTOs/UpdatePricingDTO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UdemyCarBook.DTO.PricingDTOs;
+public class UpdatePricingDTo
+{
+    public int PricingID { get; set; }
+    public string Name { get; set; }
+}
