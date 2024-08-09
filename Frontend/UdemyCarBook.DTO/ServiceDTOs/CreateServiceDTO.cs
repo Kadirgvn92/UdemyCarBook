1050afe6995fb07814a68849cc4a0217a79f5b73
Kadirgvn92 <kdrgvndeveloper@gmail.com>
2024-08-09 08:43:59 +0300

    Lesson 144 : Admin service completed

---
+++ Frontend/UdemyCarBook.DTO/ServiceDTOs/CreateServiceDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UdemyCarBook.DTO.ServiceDTOs;
+public class CreateServiceDTO
+{
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public string Icon { get; set; }
+}
--- Frontend/UdemyCarBook.WebUI/Areas/Admin/Controllers/ServiceController.cs
+++ Frontend/UdemyCarBook.WebUI/Areas/Admin/Controllers/ServiceController.cs
@@ -2,20 +2,18 @@
 using Newtonsoft.Json;
 using System.Text;
 using UdemyCarBook.DTO.AboutDTOs;
-using UdemyCarBook.DTO.AuthorDTOs;
-using UdemyCarBook.DTO.FooterAddressDTOs;
-using UdemyCarBook.DTO.LocationDTOs;
+using UdemyCarBook.DTO.ServiceDTOs;
+using UdemyCarBook.DTO.ServiceDTOs;
 
 namespace UdemyCarBook.WebUI.Areas.Admin.Controllers;
 
-
 [Area("Admin")]
 [Route("Admin/[controller]/[action]/{id?}")]
-public class LocationController : Controller
+public class ServiceController : Controller
 {
     private readonly IHttpClientFactory _httpClientFactory;
 
-    public LocationController(IHttpClientFactory httpClientFactory)
+    public ServiceController(IHttpClientFactory httpClientFactory)
     {
         _httpClientFactory = httpClientFactory;
     }
@@ -28,23 +26,23 @@ public class LocationController : Controller
     }
 
     [HttpGet]
-    public async Task<IActionResult> GetLocations()
+    public async Task<IActionResult> GetServices()
     {
         var client = _httpClientFactory.CreateClient();
-        var responseMessage = await client.GetAsync("https://localhost:44323/api/Locations");
+        var responseMessage = await client.GetAsync("https://localhost:44323/api/Services");
         if (responseMessage.IsSuccessStatusCode)
         {
             var jsonData = await responseMessage.Content.ReadAsStringAsync();
             return Content(jsonData, "application/json");
         }
-        return Json(new List<ResultAboutDTO>());
+        return Json(new List<ResultServiceDTO>());
     }
 
 
     public async Task<IActionResult> Delete(int id)
     {
         var client = _httpClientFactory.CreateClient();
-        var responseMessage = await client.DeleteAsync($"https://localhost:44323/api/Locations?id={id}");
+        var responseMessage = await client.DeleteAsync($"https://localhost:44323/api/Services?id={id}");
         if (responseMessage.IsSuccessStatusCode)
         {
             return RedirectToAction("Index");
@@ -52,40 +50,39 @@ public class LocationController : Controller
         return View();
     }
     [HttpPost]
-    public async Task<IActionResult> Create(CreateLocationDTo dTO)
+    public async Task<IActionResult> Create(CreateServiceDTO dTO)
     {
 
         var client = _httpClientFactory.CreateClient();
         var jsonData = JsonConvert.SerializeObject(dTO);
         StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
-        var responseMessage = await client.PostAsync("https://localhost:44323/api/Locations/", stringContent);
+        var responseMessage = await client.PostAsync("https://localhost:44323/api/Services/", stringContent);
         if (responseMessage.IsSuccessStatusCode)
         {
             return Ok();
         }
         return BadRequest();
     }
-
     [HttpGet]
-    public async Task<JsonResult> GetLocation(int id)
+    public async Task<JsonResult> GetService(int id)
     {
         var client = _httpClientFactory.CreateClient();
-        var responseMessage = await client.GetAsync($"https://localhost:44323/api/Locations/{id}");
+        var responseMessage = await client.GetAsync($"https://localhost:44323/api/Services/{id}");
         if (responseMessage.IsSuccessStatusCode)
         {
             var jsonData = await responseMessage.Content.ReadAsStringAsync();
-            var values = JsonConvert.DeserializeObject<UpdateLocationDTO>(jsonData);
+            var values = JsonConvert.DeserializeObject<UpdateServiceDTO>(jsonData);
             return Json(values);
         }
         return Json(null);
     }
     [HttpPost]
-    public async Task<IActionResult> Update(UpdateLocationDTO DTO)
+    public async Task<IActionResult> Update(UpdateServiceDTO DTO)
     {
         var client = _httpClientFactory.CreateClient();
         var jsonData = JsonConvert.SerializeObject(DTO);
         StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
-        var responseMessage = await client.PutAsync("https://localhost:44323/api/Locations/", stringContent);
+        var responseMessage = await client.PutAsync("https://localhost:44323/api/Services/", stringContent);
         if (responseMessage.IsSuccessStatusCode)
         {
             return RedirectToAction("Index");
