6d6ab7a8158f96cb657419b1b0fd8579d542a487
Kadirgvn92 <kdrgvndeveloper@gmail.com>
2024-08-09 09:26:29 +0300

    Lesson 147 : Admin Testimonial completed

---
+++ Frontend/UdemyCarBook.WebUI/Areas/Admin/Controllers/TestimonialController.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
+using UdemyCarBook.DTO.TestimonialDTOs;
+
+namespace UdemyCarBook.WebUI.Areas.Admin.Controllers;
+
+[Area("Admin")]
+[Route("Admin/[controller]/[action]/{id?}")]
+public class TestimonialController : Controller
+{
+    private readonly IHttpClientFactory _httpClientFactory;
+
+    public TestimonialController(IHttpClientFactory httpClientFactory)
+    {
+        _httpClientFactory = httpClientFactory;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Index()
+    {
+        return View();
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetTestimonials()
+    {
+        var client = _httpClientFactory.CreateClient();
+        var responseMessage = await client.GetAsync("https://localhost:44323/api/Testimonials");
+        if (responseMessage.IsSuccessStatusCode)
+        {
+            var jsonData = await responseMessage.Content.ReadAsStringAsync();
+            return Content(jsonData, "application/json");
+        }
+        return Json(new List<ResultTestimonialDTO>());
+    }
+
+
+    public async Task<IActionResult> Delete(int id)
+    {
+        var client = _httpClientFactory.CreateClient();
+        var responseMessage = await client.DeleteAsync($"https://localhost:44323/api/Testimonials?id={id}");
+        if (responseMessage.IsSuccessStatusCode)
+        {
+            return RedirectToAction("Index");
+        }
+        return View();
+    }
+    [HttpPost]
+    public async Task<IActionResult> Create(CreateTestimonialDTO dTO)
+    {
+
+        var client = _httpClientFactory.CreateClient();
+        var jsonData = JsonConvert.SerializeObject(dTO);
+        StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+        var responseMessage = await client.PostAsync("https://localhost:44323/api/Testimonials/", stringContent);
+        if (responseMessage.IsSuccessStatusCode)
+        {
+            return Ok();
+        }
+        return BadRequest();
+    }
+    [HttpGet]
+    public async Task<JsonResult> GetTestimonial(int id)
+    {
+        var client = _httpClientFactory.CreateClient();
+        var responseMessage = await client.GetAsync($"https://localhost:44323/api/Testimonials/{id}");
+        if (responseMessage.IsSuccessStatusCode)
+        {
+            var jsonData = await responseMessage.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<UpdateTestimonialDTO>(jsonData);
+            return Json(values);
+        }
+        return Json(null);
+    }
+    [HttpPost]
+    public async Task<IActionResult> Update(UpdateTestimonialDTO DTO)
+    {
+        var client = _httpClientFactory.CreateClient();
+        var jsonData = JsonConvert.SerializeObject(DTO);
+        StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+        var responseMessage = await client.PutAsync("https://localhost:44323/api/Testimonials/", stringContent);
+        if (responseMessage.IsSuccessStatusCode)
+        {
+            return RedirectToAction("Index");
+
+        }
+        return View();
+    }
+}
