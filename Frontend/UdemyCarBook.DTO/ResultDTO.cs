3e9de64ab7813c3cbbcf97b7dead597259eb31f6
Kadirgvn92 <kdrgvndeveloper@gmail.com>
2024-08-11 13:30:32 +0300

    Lesson 201- 203 : Reservation page updated

---
+++ Frontend/UdemyCarBook.DTO/ResultDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UdemyCarBook.DTO.CarDTOs;
+using UdemyCarBook.DTO.CarPricingDTOs;
+using UdemyCarBook.DTO.LocationDTOs;
+
+namespace UdemyCarBook.DTO;
+public class ResultDTO
+{
+    public ResultCarDTO  ResultCars { get; set; }
+    public ResultLocationDTO Locations { get; set; }
+}
--- Frontend/UdemyCarBook.WebUI/Controllers/ReservationController.cs
+++ Frontend/UdemyCarBook.WebUI/Controllers/ReservationController.cs
@@ -1,10 +1,54 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Newtonsoft.Json;
+using System.Net.Http;
+using UdemyCarBook.DTO.CarDTOs;
+using UdemyCarBook.DTO.CarPricingDTOs;
+using UdemyCarBook.DTO.LocationDTOs;
 
 namespace UdemyCarBook.WebUI.Controllers;
 public class ReservationController : Controller
 {
-    public IActionResult Index()
+    private readonly IHttpClientFactory _httpClientFactory;
+
+    public ReservationController(IHttpClientFactory httpClientFactory)
     {
+        _httpClientFactory = httpClientFactory;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        var client2 = _httpClientFactory.CreateClient();
+        var responseMessage2 = await client2.GetAsync("https://localhost:44323/api/Car/GetCarWithBrand");
+        if (responseMessage2.IsSuccessStatusCode)
+        {
+            var jsonData = await responseMessage2.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<List<ResultCarDTO>>(jsonData);
+
+            List<SelectListItem> list2 = (from x in values
+                                         select new SelectListItem
+                                         {
+                                             Text = x.BrandName,
+                                             Value = x.CarID.ToString(),
+                                         }).ToList();
+            ViewBag.Cars = list2;
+        }
+        var client = _httpClientFactory.CreateClient();
+        var responseMessage = await client.GetAsync("https://localhost:44323/api/Locations");
+        if (responseMessage.IsSuccessStatusCode)
+        {
+            var jsonData = await responseMessage.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<List<ResultLocationDTO>>(jsonData);
+
+            List<SelectListItem> list = (from x in values
+                                         select new SelectListItem
+                                         {
+                                             Text = x.Name,
+                                             Value = x.LocationID.ToString(),
+                                         }).ToList();
+            ViewBag.Location = list;
+            return View();
+        }
         return View();
     }
 }
