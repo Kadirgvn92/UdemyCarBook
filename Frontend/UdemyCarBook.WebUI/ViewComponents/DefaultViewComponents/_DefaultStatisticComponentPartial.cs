93c1b76721b4e6c64f810bb5ffc9e7cea761944e
Kadirgvn92 <kdrgvndeveloper@gmail.com>
2024-08-12 01:35:33 +0300

    Lesson 225 : FooterAddress consumed in Footer Default

--- Frontend/UdemyCarBook.WebUI/ViewComponents/DefaultViewComponents/_DefaultStatisticComponentPartial.cs
+++ Frontend/UdemyCarBook.WebUI/ViewComponents/DefaultViewComponents/_DefaultStatisticComponentPartial.cs
@@ -1,11 +1,72 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net;
+using UdemyCarBook.DTO.StatisticDTOs;
+using UdemyCarBook.WebUI.Areas.Admin.Models;
 
 namespace UdemyCarBook.WebUI.ViewComponents.DefaultViewComponents;
 
 public class _DefaultStatisticComponentPartial : ViewComponent
 {
-    public IViewComponentResult Invoke()
+    private readonly IHttpClientFactory _httpClientFactory;
+
+    public _DefaultStatisticComponentPartial(IHttpClientFactory httpClientFactory)
     {
-        return View();
+        _httpClientFactory = httpClientFactory;
+    }
+    public async Task<ResultStatisticCountDTO> GetCount(string url)
+    {
+        var client = _httpClientFactory.CreateClient();
+        var responseMessage = await client.GetAsync($"{url}");
+
+        if (responseMessage.IsSuccessStatusCode)
+        {
+            var jsonData = await responseMessage.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<ResultStatisticCountDTO>(jsonData);
+            return values;
+        }
+        return null;
+    }
+    public async Task<ResultStatisticPriceDTO> GetPrice(string url)
+    {
+        var client = _httpClientFactory.CreateClient();
+        var responseMessage = await client.GetAsync($"{url}");
+
+        if (responseMessage.IsSuccessStatusCode)
+        {
+            var jsonData = await responseMessage.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<ResultStatisticPriceDTO>(jsonData);
+            return values;
+        }
+        return null;
+    }
+    public async Task<ResultStatisticCarDTO> GetCar(string url)
+    {
+        var client = _httpClientFactory.CreateClient();
+        var responseMessage = await client.GetAsync($"{url}");
+
+        if (responseMessage.IsSuccessStatusCode)
+        {
+            var jsonData = await responseMessage.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<ResultStatisticCarDTO>(jsonData);
+            return values;
+        }
+        return null;
+    }
+
+    public async Task<IViewComponentResult> InvokeAsync()
+    {
+        var carCount = await GetCount("https://localhost:44323/api/Statistic/GetCarCount");
+        var locationCount = await GetCount("https://localhost:44323/api/Statistic/GetLocationCount");
+        var authorCount = await GetCount("https://localhost:44323/api/Statistic/GetAuhtorCount");
+        var blogCount = await GetCount("https://localhost:44323/api/Statistic/GetBlogCount");
+        var model = new StatisticViewModel
+        {
+            CarCount = carCount,
+            LocationCount = locationCount,
+            AuthorCount = authorCount,
+            BlogCount = blogCount
+        };
+        return View(model);
     }
 }
--- Frontend/UdemyCarBook.WebUI/ViewComponents/UILayoutViewComponents/_FooterComponentPartial.cs
+++ Frontend/UdemyCarBook.WebUI/ViewComponents/UILayoutViewComponents/_FooterComponentPartial.cs
@@ -1,11 +1,29 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using UdemyCarBook.DTO.BlogDTOs;
+using UdemyCarBook.DTO.FooterAddressDTOs;
 
 namespace UdemyCarBook.WebUI.ViewComponents.UILayoutViewComponents;
 
 public class _FooterComponentPartial : ViewComponent
 {
-    public IViewComponentResult Invoke()
+    private readonly IHttpClientFactory _httpClientFactory;
+
+    public _FooterComponentPartial(IHttpClientFactory httpClientFactory)
     {
+        _httpClientFactory = httpClientFactory;
+    }
+
+    public async Task<IViewComponentResult> InvokeAsync()
+    {
+        var client = _httpClientFactory.CreateClient();
+        var respond = await client.GetAsync($"https://localhost:44323/api/FooterAddresses");
+        if (respond.IsSuccessStatusCode)
+        {
+            var jsonData = await respond.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<List<ResultFooterAddressDto>>(jsonData);
+            return View(values);
+        }
         return View();
     }
 }
