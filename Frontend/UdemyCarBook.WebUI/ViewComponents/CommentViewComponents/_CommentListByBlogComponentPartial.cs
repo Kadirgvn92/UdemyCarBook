d2bba73691bbd96fe79b492eaca6989ce0f2ec2c
Kadirgvn92 <kdrgvndeveloper@gmail.com>
2024-08-12 10:44:33 +0300

    Lesson 229 - 230 : CommentList api consumed

--- Frontend/UdemyCarBook.WebUI/ViewComponents/CommentViewComponents/_CommentListByBlogComponentPartial.cs
+++ Frontend/UdemyCarBook.WebUI/ViewComponents/CommentViewComponents/_CommentListByBlogComponentPartial.cs
@@ -1,11 +1,29 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using UdemyCarBook.DTO.BlogDTOs;
+using UdemyCarBook.DTO.CommentDTOs;
 
 namespace UdemyCarBook.WebUI.ViewComponents.CommentViewComponents;
 
 public class _CommentListByBlogComponentPartial : ViewComponent
 {
-    public IViewComponentResult Invoke()
+    private readonly IHttpClientFactory _httpClientFactory;
+
+    public _CommentListByBlogComponentPartial(IHttpClientFactory httpClientFactory)
     {
+        _httpClientFactory = httpClientFactory;
+    }
+    public async Task<IViewComponentResult> InvokeAsync(int id)
+    {
+        ViewBag.Id = id;
+        var client = _httpClientFactory.CreateClient();
+        var respond = await client.GetAsync($"https://localhost:44323/api/Comment/CommentListByBlog?id={id}");
+        if (respond.IsSuccessStatusCode)
+        {
+            var jsonData = await respond.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<List<ResultCommentDTO>>(jsonData);
+            return View(values);
+        }
         return View();
     }
 }
