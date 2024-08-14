7688e8c43fe3992585287399b383e055ca497e3f
Kadirgvn92 <kdrgvndeveloper@gmail.com>
2024-08-14 22:11:14 +0300

    Lesson 255 - 258 : Dashboard completed

--- Frontend/UdemyCarBook.WebUI/Areas/Admin/Controllers/DashboardController.cs
+++ Frontend/UdemyCarBook.WebUI/Areas/Admin/Controllers/DashboardController.cs
@@ -1,4 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net.Http;
+using UdemyCarBook.DTO.CarDTOs;
+using UdemyCarBook.DTO.PricingDTOs;
 
 namespace UdemyCarBook.WebUI.Areas.Admin.Controllers;
 
@@ -6,8 +10,22 @@ namespace UdemyCarBook.WebUI.Areas.Admin.Controllers;
 [Route("Admin/[controller]/[action]/{id?}")]
 public class DashboardController : Controller
 {
-    public IActionResult Index()
+    private readonly IHttpClientFactory _httpClientFactory;
+
+    public DashboardController(IHttpClientFactory httpClientFactory)
     {
+        _httpClientFactory = httpClientFactory;
+    }
+    public async Task<IActionResult> Index()
+    {
+        var client = _httpClientFactory.CreateClient();
+        var responseMessage = await client.GetAsync("https://localhost:44323/api/Car/GetCarWithBrand");
+        if (responseMessage.IsSuccessStatusCode)
+        {
+            var jsonData = await responseMessage.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<List<ResultLast5CarsWithBrandsDto>>(jsonData);
+            return View(values);
+        }
         return View();
     }
 }
