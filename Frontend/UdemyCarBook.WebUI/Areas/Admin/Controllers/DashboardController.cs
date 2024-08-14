0c943246d579cb30c5d6f4daf954a364540ec339
Kadirgvn92 <kdrgvndeveloper@gmail.com>
2024-08-14 19:03:09 +0300

    Lesson 254 : Dashboard created

---
+++ Frontend/UdemyCarBook.WebUI/Areas/Admin/Controllers/DashboardController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UdemyCarBook.WebUI.Areas.Admin.Controllers;
+
+[Area("Admin")]
+[Route("Admin/[controller]/[action]/{id?}")]
+public class DashboardController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
