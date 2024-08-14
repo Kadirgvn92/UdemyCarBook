6bd487d16bddc7c3dfd2dc5f6642d7a3f986de6f
Kadirgvn92 <kdrgvndeveloper@gmail.com>
2024-08-14 22:23:31 +0300

    Lesson 259 - 262 : Admin Dashboard completed

---
+++ Frontend/UdemyCarBook.WebUI/Areas/Admin/ViewComponents/_AdminDashboardCardsComponentPartial.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UdemyCarBook.WebUI.Areas.Admin.ViewComponents;
+
+public class _AdminDashboardCardsComponentPartial : ViewComponent
+{
+    public IViewComponentResult Invoke()
+    {
+        return View();
+    }
+}
