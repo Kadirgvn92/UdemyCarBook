bd6456b9e284a879559ef086dd4415cbb3e78dec
Kadirgvn92 <kdrgvndeveloper@gmail.com>
2024-08-15 13:46:02 +0300

    Lesson 274 : CarDetail Comment section completed

--- Frontend/UdemyCarBook.WebUI/ViewComponents/CarDetailViewComponents/_CarDetailCommentsByCarIdComponentPartial.cs
+++ Frontend/UdemyCarBook.WebUI/ViewComponents/CarDetailViewComponents/_CarDetailCommentsByCarIdComponentPartial.cs
@@ -1,11 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using UdemyCarBook.DTO.CarDescriptionDTOs;
+using UdemyCarBook.DTO.ReviewDTOs;
 
 namespace UdemyCarBook.WebUI.ViewComponents.CarDetailViewComponents;
 
 public class _CarDetailCommentsByCarIdComponentPartial : ViewComponent
 {
-	public IViewComponentResult Invoke()
-	{
-		return View();
-	}
+    private readonly IHttpClientFactory _httpClientFactory;
+
+    public _CarDetailCommentsByCarIdComponentPartial(IHttpClientFactory httpClientFactory)
+    {
+        _httpClientFactory = httpClientFactory;
+    }
+    public async Task<IViewComponentResult> InvokeAsync(int id)
+    {
+        var client = _httpClientFactory.CreateClient();
+        var responseMessage = await client.GetAsync($"https://localhost:44323/api/Review?id={id}");
+        if (responseMessage.IsSuccessStatusCode)
+        {
+            var jsonData = await responseMessage.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<List<ResultReviewDTO>>(jsonData);
+            return View(values);
+        }
+        return View();
+    }
 }
